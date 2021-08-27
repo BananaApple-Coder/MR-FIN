f1d3497e15ab8f1b09f1e0bf4825b696e3e431e9
BananaAppleCoder <mohapatra9@gmail.com>
2021-08-26 20:57:32 -0500

    F

--- js/Game.js
+++ js/Game.js
@@ -44,6 +44,7 @@ class Game {
     form.hide();
     
     Player.getPlayerInfo();
+    player.getRank()
     
     if(allPlayers !== undefined){
       background(rgb(198,135,103));
@@ -92,7 +93,9 @@ class Game {
     }
 
     if(player.distance > 3860){
-      gameState = 2;
+      gameState = 2
+      player.rank+=1;
+      Player.updateCarsAtEnd(player.rank)
     }
    
     drawSprites();
@@ -100,5 +103,6 @@ class Game {
 
   end(){
     console.log("Game Ended");
+    console.log(player.rank)
   }
 }
